aeafcd4ebdd729ee0dcf9e4d8fae9148eda239a8
YasinZhang <zyxuan97@foxmail.com>
2021-01-05 16:49:28 +0800

    update vpn guard

--- vpn-server/files/guard.sh
+++ vpn-server/files/guard.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+lines=`cat /VPN/guard.log | wc -l`
+
+FILE="/VPN/guard.log"
+MAX_LINE=5
+
+if [ -a $FILE ]; then
+  if [[ $lines -gt $MAX_LINE ]]; then
+    echo "$FILE lines larger than $MAX_LINE"
+    echo > $FILE
+  fi
+fi
+
 STATUS=`expressvpn status`
 
 if [[ "$STATUS" =~ "Connected to" ]];then
